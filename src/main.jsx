7a28e3f61a819f5a63e944e507fb9acf67818994
jmkim <rookie2000g@gmail.com>
2022-08-22 16:10:08 +0900

    Change: remove code based on react-modal

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import ReactModal from "react-modal";
 import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "./customTheme";
 
@@ -16,5 +15,3 @@ root.render(
     </BrowserRouter>
   </StrictMode>,
 );
-
-ReactModal.setAppElement("#root");
