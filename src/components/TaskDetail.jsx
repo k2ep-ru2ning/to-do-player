1f2c498ec4ca048af1df31be9d7afb09d111a024
jmkim <rookie2000g@gmail.com>
2022-08-30 16:23:41 +0900

    Change(TaskDetail): use chakra-ui for styling

--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -1,7 +1,18 @@
+import { Flex, Text } from "@chakra-ui/react";
+
 export default function TaskDetail() {
   return (
-    <div className="border border-gray-300 rounded-md">
-      <div className="px-4 py-10 text-gray-700 text-center">할 일 목록에서 할 일을 선택하세요.</div>
-    </div>
+    <Flex
+      direction="column"
+      justifyContent="center"
+      height={80}
+      p={4}
+      borderWidth="1px"
+      borderRadius="lg"
+    >
+      <Text fontSize="lg" color="gray.700" px={2} py={4} textAlign="center">
+        할 일 목록에서 할 일을 선택하세요.
+      </Text>
+    </Flex>
   );
 }
