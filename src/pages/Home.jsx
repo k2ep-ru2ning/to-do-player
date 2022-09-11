cea8e21f2c92d08114966d96fe0ca8f1b27e4177
jmkim <rookie2000g@gmail.com>
2022-09-11 21:38:05 +0900

    Feat(Home): add animation to keywords

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { Button, Center, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Center, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 export default function Home() {
@@ -6,30 +7,30 @@ export default function Home() {
     <Center p={6} minH="100vh">
       <SimpleGrid columns={{ md: 2 }} gap={{ base: 8, lg: 16 }} placeItems="center">
         <VStack spacing={0}>
-          <Text
-            bgGradient="linear(to-r, #6300ff, #ff0086)"
-            bgClip="text"
-            fontSize={{ base: "7xl", lg: "8xl", xl: "9xl" }}
-            fontWeight="extrabold"
-          >
-            Focus.
-          </Text>
-          <Text
-            bgGradient="linear(to-r, #00beff, #0027ff)"
-            bgClip="text"
-            fontSize={{ base: "7xl", lg: "8xl", xl: "9xl" }}
-            fontWeight="extrabold"
-          >
-            Plan.
-          </Text>
-          <Text
-            bgGradient="linear(to-r, #ff8f00, #feff00)"
-            bgClip="text"
-            fontSize={{ base: "7xl", lg: "8xl", xl: "9xl" }}
-            fontWeight="extrabold"
-          >
-            Start.
-          </Text>
+          {keywords.map((keyword, index) => (
+            <Box
+              key={keyword.text}
+              bgGradient={keyword.backgroundGradient}
+              bgClip="text"
+              fontSize={{ base: "7xl", lg: "8xl", xl: "9xl" }}
+              fontWeight="extrabold"
+            >
+              <motion.div
+                animate={{
+                  color: ["rgba(0,0,0,1)", "rgba(0,0,0,0)", "rgba(0,0,0,0)", "rgba(0,0,0,1)"],
+                }}
+                transition={{
+                  delay: 3 * index,
+                  repeat: Infinity,
+                  repeatDelay: 6,
+                  duration: 3,
+                  times: [0, 0.15, 0.85, 1],
+                }}
+              >
+                {keyword.text}
+              </motion.div>
+            </Box>
+          ))}
         </VStack>
         <VStack spacing={8}>
           <VStack spacing={{ base: 2, md: 4 }}>
@@ -48,3 +49,18 @@ export default function Home() {
     </Center>
   );
 }
+
+const keywords = [
+  {
+    text: "Focus.",
+    backgroundGradient: "linear(to-r, #6300ff, #ff0086)",
+  },
+  {
+    text: "Plan.",
+    backgroundGradient: "linear(to-r, #00beff, #0027ff)",
+  },
+  {
+    text: "Start.",
+    backgroundGradient: "linear(to-r, #ff8f00, #feff00)",
+  },
+];
