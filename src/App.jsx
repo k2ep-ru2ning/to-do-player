5df32617ec333d15db137d68bf681a21b00120b4
jmkim <rookie2000g@gmail.com>
2022-10-03 10:54:28 +0900

    Feat: add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Main from "./pages/Main";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/main" element={<Main />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+import { Button, Center, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <Center bgColor="gray.50" minH="100vh">
+      <VStack spacing={8} px={4}>
+        <VStack>
+          <Text fontSize="6xl" fontWeight="bold" color="main.500">
+            404
+          </Text>
+          <Text fontSize="3xl" fontWeight="bold" textAlign="center">
+            해당 페이지를 찾지 못했습니다.
+          </Text>
+        </VStack>
+        <Button as={Link} to="/main" colorScheme="main" variant="link" size="lg">
+          메인 페이지로 이동
+        </Button>
+      </VStack>
+    </Center>
+  );
+}
