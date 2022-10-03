25fb03c748de46d6d81ba16f7472dc9f0a567f64
jmkim <rookie2000g@gmail.com>
2022-10-03 11:13:28 +0900

    Design(HeaderBar): reduce header bar height and title font size

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -2,8 +2,8 @@ import { Center, Text } from "@chakra-ui/react";
 
 export default function HeaderBar() {
   return (
-    <Center as="header" h="24">
-      <Text as="span" color="main.500" fontSize="4xl" fontWeight="bold">
+    <Center as="header" h="20">
+      <Text as="span" fontSize="4xl" fontWeight="bold">
         FPS
       </Text>
     </Center>
