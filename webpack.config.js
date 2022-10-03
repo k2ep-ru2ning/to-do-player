35ab929bf9ff267bccddebf57c347429cf0fd8a1
jmkim <rookie2000g@gmail.com>
2022-10-03 17:18:14 +0900

    Feat: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/index.html",
+      favicon: "public/favicon.ico",
     }),
     isDevelopment && new ReactRefreshWebpackPlugin(),
     !isDevelopment &&
