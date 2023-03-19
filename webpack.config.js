50038c1deeb18057f017675fefe8c69f524741d8
jmkim <rookie2000g@gmail.com>
2023-03-19 17:07:28 +0900

    Remove unused webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,34 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const DEV_MODE = "development";
-const PROD_MODE = "production";
-const isDevelopment = process.env.NODE_ENV !== PROD_MODE;
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  name: "FPS - TodoList and Timer App",
-  mode: isDevelopment ? DEV_MODE : PROD_MODE,
+  mode: isDevelopment ? "development" : "production",
   devtool: isDevelopment ? "eval-cheap-module-source-map" : false,
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js", ".jsx", "..."],
   },
   entry: {
-    app: "./src/main",
+    main: "./src/main",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "assets/[name].js",
+    filename: "[name].js",
     clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/i,
         exclude: /node_modules/,
         use: [
           {
             loader: "babel-loader",
             options: {
               presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    useBuiltIns: "usage",
-                    corejs: "3",
-                  },
-                ],
+                "@babel/preset-env",
                 [
                   "@babel/preset-react",
                   { runtime: "automatic", importSource: "@emotion/react" },
@@ -52,29 +42,20 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.css$/,
-        use: [
-          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-        ],
-      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html",
-      favicon: "public/favicon.ico",
+      template: "template/index.html",
+      favicon: "template/favicon.ico",
     }),
     isDevelopment && new ReactRefreshWebpackPlugin(),
-    !isDevelopment &&
-      new MiniCssExtractPlugin({
-        filename: "assets/[name].css",
-      }),
   ].filter(Boolean),
   devServer: {
+    static: "./dist",
     port: 3000,
     historyApiFallback: true,
     open: true,
+    hot: true,
   },
 };
